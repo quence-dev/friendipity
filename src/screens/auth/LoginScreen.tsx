7ab3db972c2ca991ecb8db413f92f74765272620
Spencer <quence@bu.edu>
2026-01-14 20:00:53 -0500

    some login screen progress

--- src/screens/auth/LoginScreen.tsx
+++ src/screens/auth/LoginScreen.tsx
@@ -1,9 +1,65 @@
-import { View, Text } from 'react-native';
+import { useState } from 'react';
+import { View, Text, TextInput, StyleSheet } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { loginSchema } from '@/schemas';
+
+function isValidEmail(email: string): boolean {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
 
 export default function LoginScreen() {
+  const [email, setEmail] = useState('');
+
+  async function validateEmail() {
+    try {
+
+    } catch(error) {
+
+    }
+  }
+
   return (
-    <View>
-      <Text>This will be the login screen.</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text}>This will be the login screen.</Text>
+      <TextInput autoComplete='email'
+        onChangeText={setEmail}
+        value={email}
+        placeholder='johndoe@gmail.com'
+        />
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  },
+  title: {
+    fontSize: 36,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  text: {
+    fontSize: 18,
+    color: '#000',
+    marginBottom: 10,
+  },
+  subtitle: {
+    fontSize: 18,
+    color: '#666',
+    marginBottom: 10,
+  },
+  info: {
+    fontSize: 14,
+    color: '#999',
+    marginBottom: 5,
+  },
+  buttonContainer: {
+    marginTop: 20,
+  },
+});
